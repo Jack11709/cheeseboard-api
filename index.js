748209e6f3164041bb0300907098318b1cac3c8a
Mike Hayden <mike.hayden@ga.co>
2018-11-23 11:52:21 +0000

    Adding CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 mongoose.Promise = require('bluebird');
 const { port, dbURI } = require('./config/environment');
 const routes = require('./config/routes');
@@ -9,6 +10,8 @@ const errorHandler = require('./lib/errorHandler');
 const app = express();
 
 mongoose.connect(dbURI);
+
+app.use(cors({ origin: /http:\/\/localhost:[0-9]{2,4}$/ }));
 
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
